0b55087e7efc187c7bac119f53a5dc473d6ccbb0
K Mahidhar <143922729+Khandavilli-Mahidhar93@users.noreply.github.com>
2024-04-19 16:55:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	let inp = query.selectAll("input");
+	let f_name=inp[0].value
+	let l_name=inp[1].value
+	let name=f_name+ " " + l_name
+	alert(name)
 
 }
